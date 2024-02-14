2e7b407f5a1692b10964873a17233308bfc16517
Aevismotax1 <108089675+AevisMotax@users.noreply.github.com>
2024-02-14 18:05:15 -0500

    UI -based on someone else
    -Juba Laiche

---
+++ UI/HomePage.php
@@ -0,0 +1,20 @@
+<?php
+echo'<!DOCTYPE html>
+<html>
+<th><a href="index.php"><button >Index</button></a></th>
+<th><a href="Student.php"> <button >Student</button></a></th>
+    <th><a href="Employee.php"> <button >Employee</button></a></th>
+    <th><a href="Facility.php"><button >Facility</button></a></th>
+    <th><a href="Infection.php"><button >Infection</button></a></th>
+    <th><a href="Vaccination.php"><button >Vaccination</button></a></th>
+    <th><a href="Registration.php"><button >Registration</button></a></th>
+    <th><a href="email.php"><button >Email</button></a></th><h1>Employee</h1>
+
+<body>
+<a href="createEmp.php"><button>Create Employee</button></a>
+<a href="displayEmp.php"><button>Display Employee</button></a>
+<a href="schedule.php"><button>Show Schedule</button></a>
+</body>
+
+</html>';
+?>
