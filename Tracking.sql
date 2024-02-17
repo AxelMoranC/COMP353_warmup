d702f90ed706cdb7e544fcc6a200d45720bfda5b
Aevismotax1 <108089675+AevisMotax@users.noreply.github.com>
2024-02-17 00:15:56 -0500

    Update Tracking.sql

--- Tracking.sql
+++ Tracking.sql
@@ -1,2 +1,32 @@
 # i)
+SELECT 
+    F.Province, F.FacilityName, F.Capacity,
+    COUNT(E.EmployeeID),
+    COUNT(DISTINCT L.PersonID)
+FROM Facilities F
+JOIN Employees E ON F.FacilityID = E.FacilityID
+JOIN LivesWithEmployees L ON E.EmployeeID = L.EmployeeID
+GROUP BY F.Province, F.FacilityName, F.Capacity;
 
+
+# ii)
+
+
+# iii)
+
+
+# iv)
+
+
+# v)
+
+
+
+#vi)
+
+
+
+# vii)
+
+
+# viii)
