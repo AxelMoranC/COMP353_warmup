12ebced271399e7f553a846f96108da87007a07f
Robertmotax1 <robertz_chen@outlook.com>
2024-04-01 22:08:44 -0400

    added facility

--- UI/HomePage.php
+++ UI/HomePage.php
@@ -1,6 +1,7 @@
-<?php
-echo'<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
+
+<head>
 <th><a href="index.php"><button >Index</button></a></th>
 <th><a href="Student.php"> <button >Student</button></a></th>
     <th><a href="Employee.php"> <button >Employee</button></a></th>
@@ -9,12 +10,12 @@ echo'<!DOCTYPE html>
     <th><a href="Vaccination.php"><button >Vaccination</button></a></th>
     <th><a href="Registration.php"><button >Registration</button></a></th>
     <th><a href="email.php"><button >Email</button></a></th><h1>Employee</h1>
+</head>
 
 <body>
-<a href="createEmp.php"><button>Create Employee</button></a>
-<a href="displayEmp.php"><button>Display Employee</button></a>
+<a href="createEmployee.php"><button>Create Employee</button></a>
+<a href="displayEmployee.php"><button>Display Employee</button></a>
 <a href="schedule.php"><button>Show Schedule</button></a>
 </body>
 
 </html>';
-?>
---
+++ UI/facility.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<th><a href="index.php"><button >Index</button></a></th>
+<th><a href="Student.php"> <button >Student</button></a></th>
+    <th><a href="Employee.php"> <button >Employee</button></a></th>
+    <th><a href="Facility.php"><button >Facility</button></a></th>
+    <th><a href="Infection.php"><button >Infection</button></a></th>
+    <th><a href="Vaccination.php"><button >Vaccination</button></a></th>
+    <th><a href="Registration.php"><button >Registration</button></a></th>
+    <th><a href="email.php"><button >Email</button></a></th>
+</head>
+
+<body>
+<h1>Facility</h1>
+
+<a href="createFacility.php"><button>Create Facility</button></a>
+<a href="displayFacility.php"><button>Display Facilities</button></a>
+
+</body>
+
+</html>';
