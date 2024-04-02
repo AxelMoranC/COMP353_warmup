e227f6a70911d72d7edb4dd0e02e5ce3b5b821b1
Aevismotax1 <108089675+AevisMotax@users.noreply.github.com>
2024-04-01 22:39:09 -0400

    Update createFacility.php

--- UI/createFacility.php
+++ UI/createFacility.php
@@ -9,7 +9,8 @@ $statement->execute();
 <html lang="en">
 
 <head>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Facilities</title>
 </head>
 
