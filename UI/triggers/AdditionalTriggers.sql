d0eb82811bcfe1b04c10d9876a03dd9ed1c26765
Robertmotax1 <robertz_chen@outlook.com>
2024-04-06 09:09:03 -0400

    add changes in triggers

--- UI/triggers/AdditionalTriggers.sql
+++ UI/triggers/AdditionalTriggers.sql
@@ -2,35 +2,37 @@
 -- Trigger used to ensure that a Person is connected to a good ResidenceID and not some random
 
 DELIMITER $$
-CREATE TRIGGER before_insert_person
-BEFORE INSERT ON Persons
-FOR EACH ROW
-BEGIN
-    DECLARE residence_count INT DEFAULT 0;
-    
-    SET residence_count = SELECT (COUNT(*) INTO residence_count FROM Residence WHERE ResidenceID = NEW.ResidenceID);
-    
-    IF residence_count = 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ResidenceID does not exist in Residence table';
-    END IF;
-END$$
+    CREATE TRIGGER before_insert_person
+    BEFORE INSERT ON Persons
+    FOR EACH ROW
+    BEGIN
+        DECLARE residence_count INT DEFAULT 0;
+        
+        SET residence_count = SELECT (COUNT(*) INTO residence_count FROM Residence WHERE ResidenceID = NEW.ResidenceID);
+        
+        IF residence_count = 0 THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ResidenceID does not exist in Residence table';
+        END IF;
+    END$$
 DELIMITER ;
 
+DROP TRIGGER IF EXISTS before_insert_person;
 
 -- trigger to ensure an Employee is connected to a good FacilityID and not some random shenanigans
 DELIMITER $$
-CREATE TRIGGER before_insert_employee
-BEFORE INSERT ON Employees
-FOR EACH ROW
-BEGIN
-    DECLARE facility_id INT DEFAULT 0;
-    
-    SET facility_id = (SELECT COUNT(*) INTO facility_id 
-                        FROM Facility WHERE FacilityID = NEW.FacilityID);
-    
-    IF (facility_id = 0 OR facility_id < 0 ) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ResidenceID does not exist in Residence table';
-    END IF;
-END$$
+    CREATE TRIGGER before_insert_employee
+    BEFORE INSERT ON Employees
+    FOR EACH ROW
+    BEGIN
+        DECLARE facility_id INT DEFAULT 0;
+        
+        SET facility_id = (SELECT COUNT(*) INTO facility_id 
+                            FROM Facility WHERE FacilityID = NEW.FacilityID);
+        
+        IF (facility_id = 0 OR facility_id < 0 ) THEN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ResidenceID does not exist in Residence table';
+        END IF;
+    END$$
 DELIMITER ;
 
+DROP TRIGGER IF EXISTS before_insert_employee;
